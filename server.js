856738eadfe6fe159f965632bce636ffc807f57e
andreszapataq <andreszapataq@gmail.com>
2022-09-16 23:56:27 -0500

    first route

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
-const port = 5000
+const port = process.env.PORT || 5001
 
 const app = express()
+
+app.get('/api/goals', (req, res) => {
+    res.json({ message: 'Get goals' })
+})
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
