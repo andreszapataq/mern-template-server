ce2a3e30b2a5f68fd7d9a677853ec2431c2a3d20
andreszapataq <andreszapataq@gmail.com>
2022-09-27 17:11:02 -0500

    initial REST API for data

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -1,10 +1,14 @@
 const asyncHandler = require('express-async-handler')
 
+const Data = require('../models/dataModel')
+
 // @desc    Get data
 // @route   GET /api/data
 // @access  Private
 const getData = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Get data' })
+    const data = await Data.find()
+
+    res.status(200).json(data)
 })
 
 // @desc    Set data
@@ -16,21 +20,45 @@ const setData = asyncHandler(async (req, res) => {
         throw new Error('Please add a text field')
     }
 
-    res.status(200).json({ message: 'Set data' })
+    const data = await Data.create({
+        text: req.body.text
+    })
+
+    res.status(200).json(data)
 })
 
 // @desc    Update data
 // @route   PUT /api/data/:id
 // @access  Private
 const updateData = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Update data ${req.params.id}` })
+    const data = await Data.findById(req.params.id)
+
+    if(!data) {
+        res.status(400)
+        throw new Error('Data not found')
+    }
+
+    const updatedData = await Data.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
+    })
+
+    res.status(200).json(updatedData)
 })
 
 // @desc    Delete data
 // @route   DELETE /api/data/:id
 // @access  Private
 const deleteData = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Delete data ${req.params.id}` })
+    const data = await Data.findById(req.params.id)
+
+    if(!data) {
+        res.status(400)
+        throw new Error('Data not found')
+    }
+
+    await data.remove()
+
+    res.status(200).json({ id: req.params.id })
 })
 
 module.exports = {
