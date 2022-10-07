917f0b4f4e5cc8d85dcd52e90b404337d2daef95
andreszapataq <andreszapataq@gmail.com>
2022-10-06 22:57:15 -0500

    user routes and controller

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/data', require('./routes/dataRoutes'))
+app.use('/api/users', require('./routes/userRoutes'))
 
 app.use(errorHandler)
 
