751f4c3ea7cd40a8af5231f69148511c0e6a43e9
andreszapataq <andreszapataq@gmail.com>
2022-10-16 13:08:51 -0500

    add asyncHandler to userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,26 +1,28 @@
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
 
 // @desc    Register new user
 // @route   POST /api/users
 // @access  Public
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
     res.json({ message: 'Register user' })
-}
+})
 
 // @desc    Authenticate a user
 // @route   POST /api/users/login
 // @access  Public
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
     res.json({ message: 'Login user' })
-}
+})
 
 // @desc    Ger user data
 // @route   GET /api/users/me
 // @access  Public
-const getMe = (req, res) => {
+const getMe = asyncHandler(async (req, res) => {
     res.json({ message: 'This is the user data' })
-}
+})
 
 module.exports = {
     registerUser,
