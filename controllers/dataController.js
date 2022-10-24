bcca1977577e0a206a5c1b8b48fa5db3ca205174
andreszapataq <andreszapataq@gmail.com>
2022-10-23 19:37:08 -0500

    finish JWT authentication

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -1,12 +1,13 @@
 const asyncHandler = require('express-async-handler')
 
 const Data = require('../models/dataModel')
+const User = require('../models/userModel')
 
 // @desc    Get data
 // @route   GET /api/data
 // @access  Private
 const getData = asyncHandler(async (req, res) => {
-    const data = await Data.find()
+    const data = await Data.find({ user: req.user.id })
 
     res.status(200).json(data)
 })
@@ -21,7 +22,8 @@ const setData = asyncHandler(async (req, res) => {
     }
 
     const data = await Data.create({
-        text: req.body.text
+        text: req.body.text,
+        user: req.user.id
     })
 
     res.status(200).json(data)
@@ -36,6 +38,20 @@ const updateData = asyncHandler(async (req, res) => {
     if(!data) {
         res.status(400)
         throw new Error('Data not found')
+    }
+
+    const user = await User.findById(req.user.id)
+
+    // Check for user
+    if(!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    // Make sure the logged in user matches the data user
+    if(data.user.toString() !== user.id) {
+        res.status(401)
+        throw new Error('User not authorized')
     }
 
     const updatedData = await Data.findByIdAndUpdate(req.params.id, req.body, {
@@ -54,6 +70,20 @@ const deleteData = asyncHandler(async (req, res) => {
     if(!data) {
         res.status(400)
         throw new Error('Data not found')
+    }
+
+    const user = await User.findById(req.user.id)
+
+    // Check for user
+    if(!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    // Make sure the logged in user matches the data user
+    if(data.user.toString() !== user.id) {
+        res.status(401)
+        throw new Error('User not authorized')
     }
 
     await data.remove()
